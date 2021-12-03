4a18225822094c8453651dd7e66156906af210c2
EvgeniyaNesterova <jane1990@mail.ru>
2021-12-03 12:22:09 +0300

    create: mobile version for navbar

--- js/main.js
+++ js/main.js
@@ -33,3 +33,9 @@ $(window).scroll(function() {
   var st = $(this).scrollTop();
   console.log(st); 
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log('Клик по кнопке меню');
+  document.querySelector(".navbar-menu").classList.toggle('navbar-menu--visible');
+});
