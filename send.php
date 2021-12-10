5d176513b6422c415276ae16a5b3b8653d6009c7
EvgeniyaNesterova <jane1990@mail.ru>
2021-12-10 21:41:54 +0300

    fix: php mailer

--- send.php
+++ send.php
@@ -5,36 +5,43 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 // Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$email = $_POST['email'];
+$name_booking = $_POST['name_booking'];
+$phone_booking = $_POST['phone_booking'];
+$message_booking = $_POST['message_booking'];
+$email_booking = $_POST['email_booking'];
+$email_newsletter = $_POST['email_newsletter'];
+$name_footer = $_POST['name_footer'];
+$phone_footer = $_POST['phone_footer'];
+$message_footer = $_POST['message_footer'];
+
+
 
 // Формирование самого письма
-$title = "Booking request Best Tour Plan";
+$title_booking = "Booking request Best Tour Plan";
 $title_newsletter = "Subscription to Newsletters from Best Tour Plan";
-$body = "
+$title_footer = "New message";
+$body_booking = "
 <h4>Sent from: evgeniya.nesterova0804@gmail.com </h3>
 <h4>Web-site: Best Tour Plan</h3>
 <h2>Booking request</h2>
-<b>Имя:</b> $name<br>
-<b>Почта:</b> $email<br><br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<b>Имя:</b> $name_booking<br>
+<b>Почта:</b> $email_booking<br><br>
+<b>Телефон:</b> $phone_booking<br><br>
+<b>Сообщение:</b><br>$message_booking
 ";
 $body_newsletter = "
 <h4>Sent from: evgeniya.nesterova0804@gmail.com </h3>
 <h4>Web-site: Best Tour Plan</h3>
 <h2>I would like to subscribe to Newsletters from Best Tour Plan</h2>
-<b>Почта:</b> $email<br><br>
+<b>Почта:</b> $email_newsletter<br><br>
 ";
 $body_footer = "
 <h4>Sent from: evgeniya.nesterova0804@gmail.com </h3>
 <h4>Web-site: Best Tour Plan</h3>
 <h2>New message</h2>
-<b>Имя:</b> $name<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<b>Имя:</b> $name_footer<br>
+<b>Телефон:</b> $phone_footer<br><br>
+<b>Сообщение:</b><br>$message_footer
 ";
 
 // Настройки PHPMailer
@@ -68,18 +75,18 @@ try {
     //     $mail->Body = $body_newsletter;
     //     $mail->Subject = $title_newsletter;}
     
-    if(isset($name, $email, $phone, $message)) {
-        $mail->Body = $body;
+    if(isset($_POST['name_booking'], $_POST['email_booking'], $_POST['phone_booking'], $_POST['message_booking'])) {
+        $mail->Subject = $title_booking;
+        $mail->Body = $body_booking;
     }
-    if(isset($name, $phone, $message)) {
-        $mail->Body = $body_footer;
-    }
-    if(isset($email)) {
+    if(isset($_POST['email_newsletter'])) {
+        $mail->Subject = $title_newsletter;
         $mail->Body = $body_newsletter;
     }
-
-
-
+    if(isset($_POST['name_footer'], $_POST['phone_footer'], $_POST['message_footer'])) {
+        $mail->Subject = $title_footer;
+        $mail->Body = $body_footer;
+    }
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
